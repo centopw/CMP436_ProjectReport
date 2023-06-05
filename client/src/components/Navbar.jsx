66ad218a73d1cd2c672f0a70dee5cf45f2461865
cento <tanhiep26@gmail.com>
2023-06-05 07:28:10 +0700

    refactor: remove dropdown menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React } from "react";
 import styled from "styled-components";
 import { mobile } from "../responsive";
 import { useSelector } from "react-redux";
@@ -44,25 +44,8 @@ const Right = styled.div`
 
 const Logo = styled.h1`
   text-decoration: none;
-`
-const DropdownMenu = styled.div`
-  position: absolute;
-  top: 60px;
-  right: 0px;
-  width: 100px;
-  height: 100px;
-  background-color: white;
-  z-index: 1;
-  ${mobile({ top: "50px", right: "0px" })}
 `;
-const DropdownItem = styled.div`
-  cursor: pointer;
-  padding: 10px;
-  &:hover {
-    background-color: #f1f1f1;
-  }
 
-`;
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
@@ -75,15 +58,10 @@ const MenuItem = styled.div`
 const Navbar = () => {
   const currentUser = useSelector((state) => state.user.currentUser);
   const quantity = useSelector(state => state.cart.quantity)
-  const [dropdownOpen, setDropdownOpen] = useState(false);
   const dispatch = useDispatch();
 
   const handleLogout = () => {
     dispatch(logout());
-  };
-
-  const toggleDropdown = () => {
-    setDropdownOpen((prevState) => !prevState);
   };
 
   return (
@@ -110,16 +88,10 @@ const Navbar = () => {
         </Center>
         <Right>
         {currentUser ? (
-            <div>
-              <MenuItem onClick={toggleDropdown}>
-                {currentUser.username}
-              </MenuItem>
-              {dropdownOpen && (
-                <DropdownMenu>
-                  <DropdownItem onClick={handleLogout}>Logout</DropdownItem>
-                </DropdownMenu>
-              )}
-            </div>
+            <>
+              <MenuItem>{currentUser.username}</MenuItem>
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </>
           ) : (
             <>
               <Link to="/register">
